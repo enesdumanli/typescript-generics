2b6feda5a81743b746d738cde8c6a809dd98f771
Enes Dumanlı <57552112+enesdumanli@users.noreply.github.com>
2022-07-01 17:17:18 +0300

    generics.ts

---
+++ generics.ts
@@ -0,0 +1,39 @@
+function identity2(arg: string[]) : string {
+    return arg.join('')
+}
+
+const output1 = identity2(['enes','dumanli'])
+console.log(output1)
+
+
+
+interface GenericIdentityFn<Type> {
+  (arg: Type): Type;
+}
+ 
+function identity<Type>(arg: Type): string {
+  typeof arg !== 'string' 
+    ? 
+  return 'not equal string type' 
+    : 
+  return arg
+}
+ 
+let myIdentity: GenericIdentityFn<string> = identity;
+
+console.log(myIdentity('deneme'))
+
+
+interface SomeConstraints {
+  length: number;
+  join(): string;
+}
+ 
+function loggingIdentity<Type extends SomeConstraints>(arg: Type): Type {
+  console.log(arg.length); // Now we know it has a .length property, so no more error
+  console.log(arg.join())
+  return arg;
+}
+
+loggingIdentity(['enes','dumanlı'])
+
